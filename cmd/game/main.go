ed36dbd5373c3aed1d039d596f722e14c19d0ad6
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2026-02-24 22:25:27 +0100

    Remove scene package and usage from main

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -3,16 +3,13 @@ package main
 import (
 	"game-engine/internal/graphics"
 	"game-engine/internal/logger"
-	"game-engine/internal/scene"
 	"game-engine/internal/terminal"
 )
 
 func main() {
 	log := logger.New()
 	term := terminal.New(log)
-	scn := scene.New()
 	draw := func() {
-		scn.Draw()
 		term.Draw()
 	}
 	graphics.Run(term.Update, draw)
