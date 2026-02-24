742fa654589e8ec7e1ad3e906410ce8486486976
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2026-02-24 22:31:32 +0100

    Wire scene into main (update and draw order)

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"game-engine/internal/graphics"
 	"game-engine/internal/logger"
+	"game-engine/internal/scene"
 	"game-engine/internal/terminal"
 )
 
 func main() {
 	log := logger.New()
 	term := terminal.New(log)
+	scn := scene.New()
+	update := func() {
+		scn.Update()
+		term.Update()
+	}
 	draw := func() {
+		scn.Draw()
 		term.Draw()
 	}
-	graphics.Run(term.Update, draw)
+	graphics.Run(update, draw)
 }
